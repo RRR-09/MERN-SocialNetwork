f20480f037890053452e4dcffdbb059c0c200d63
RusinR <79768142+RusinR@users.noreply.github.com>
2021-07-27 23:17:57 -0400

    Preliminary user registration MongoDB submission

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
 const { check, validationResult } = require('express-validator');
+
+const User = require('../../models/User');
 
 //@route    POST api/users
 //@desc     Register user
@@ -15,14 +19,50 @@ router.post(
       'Please use a password with 6 or more characters'
     ).isLength({ min: 6 }),
   ],
-  (req, res) => {
-    console.log(req.body);
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       //return bad response with any errors in the response json
       return res.status(400).json({ errors: errors.array() });
     }
-    res.send('User Route');
+    const { name, email, password } = req.body;
+    try {
+      //See if user exists
+      let user = await User.findOne({ email });
+
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'User already exists' }] });
+      }
+
+      //Get users gravatar
+      const avatar = gravatar.url(email, {
+        s: '200', //size in px
+        r: 'pg', //no NSFW avatars
+        d: 'mm', //default gravatar if not found
+      });
+
+      user = new User({
+        name,
+        email,
+        avatar,
+        password,
+      });
+
+      //Encrypt Password
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      //Return JWT
+      res.send('User Registered');
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
   }
 );
 
