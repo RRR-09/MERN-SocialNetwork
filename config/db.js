3fc4ba3a25f80b2af4862e6308509f6044871bdd
RusinR <79768142+RusinR@users.noreply.github.com>
2021-07-27 21:47:45 -0400

    Basic database connection

---
+++ config/db.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+const mongoURI = process.env.MONGO_URI;
+//const config = require('config');
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(mongoURI, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    });
+    console.log('MongoDB Connected');
+  } catch (err) {
+    console.error(err.message);
+    //exit process with failure
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const express = require('express');
+const connectDB = require('./config/db.js');
+
 const app = express();
+//Initialize DB Connection
+connectDB();
+
 app.get('/', (_req, res) => res.send('API Running'));
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
