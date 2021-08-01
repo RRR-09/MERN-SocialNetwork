3adcf4a662a0590aeeac81e3e4e0d2dbd47edf6f
RusinR <79768142+RusinR@users.noreply.github.com>
2021-07-31 21:28:31 -0400

    removed unneeded import

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,6 +1,4 @@
-import React from 'react';
-import { Fragment, useState } from 'react';
-import axios from 'axios';
+import React, { Fragment, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const Login = () => {
