39bdc733633066be0b4844155ec57e7e89386e8b
RusinR <79768142+RusinR@users.noreply.github.com>
2021-08-14 20:41:15 -0400

    Added github repos to profile, fixed sec issue

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -7,6 +7,7 @@ import ProfileTop from './ProfileTop';
 import ProfileAbout from './ProfileAbout';
 import ProfileExperience from './ProfileExperience';
 import ProfileEducation from './ProfileEducation';
+import ProfileGithub from './ProfileGithub';
 import { getProfileById } from '../../actions/profile';
 
 const Profile = ({
@@ -67,6 +68,9 @@ const Profile = ({
                 <h4>No education credentials</h4>
               )}
             </div>
+            {profile.githubUsername && (
+              <ProfileGithub username={profile.githubUsername} />
+            )}
           </div>
         </Fragment>
       )}
