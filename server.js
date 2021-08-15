05927abbf16993c3146dba2c5fe88a751f7e558a
RusinR <79768142+RusinR@users.noreply.github.com>
2021-08-14 23:42:17 -0400

    Pre-deploy changes

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db.js');
+const path = require('path');
 
 const app = express();
 //Initialize DB Connection
@@ -7,13 +8,22 @@ connectDB();
 //Initialize middleware
 app.use(express.json());
 
-app.get('/', (_req, res) => res.send('API Running'));
+//Localhost
+//app.get('/', (_req, res) => res.send('API Running'));
 
 //Define routes
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/posts', require('./routes/api/posts'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/users', require('./routes/api/users'));
+
+//Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
